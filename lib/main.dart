3ad558372ecb01c502e4c3e59a6e30b30bcd7d08
Artem GartService <art.garanskiy@gmail.com>
2023-01-28 07:18:34 +0800

    colorScheme used

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,9 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        colorScheme: ColorScheme.light(
+          primary: Color(0xFF0A0E21),
+        ),
 
       ),
       home: InputPage(),
