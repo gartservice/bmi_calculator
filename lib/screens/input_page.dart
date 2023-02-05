309839ec0fead751af6a3662e9e86b3d8a4b08fd
Artem GartService <art.garanskiy@gmail.com>
2023-02-06 06:55:17 +0800

    Screens files moved to new folder

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'components/icon_content.dart';
-import 'components/reusable_card.dart';
-import 'constants.dart';
-import 'components/bottom_button.dart';
-import 'components/round_icon_button.dart';
+import 'package:bmi_calculator/components/icon_content.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
+import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/components/bottom_button.dart';
+import 'package:bmi_calculator/components/round_icon_button.dart';
 
 enum Gender {
   male,
