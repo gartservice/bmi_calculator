a6bfe8f4784fc719992b47254c1ec7cc6afda9ca
Artem GartService <art.garanskiy@gmail.com>
2023-02-05 08:10:02 +0800

    new constants was added

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,7 +5,7 @@ const kInactiveCardColor = Color(0xFF111328);
 const kActiveCardColour = Color(0xFF1D1E33);
 const kBottomContainerColour = Color(0xFFEB1555);
 
-const kLabelTextStyle =  TextStyle(
+const kLabelTextStyle = TextStyle(
   fontSize: 15.0,
   color: Color(0xFF8D8E98),
 );
@@ -21,3 +21,22 @@ const kMinHeight = 120;
 const kMaxHeight = 230;
 const kSliderActiveColor = Color(0xFFEB1555);
 const kSliderInactiveColor = Color(0xFF8D8E98);
+
+//Constants for result page
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+const kBMITextStyle = TextStyle(
+  // color: Color(0xFF24D876),
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0
+);
